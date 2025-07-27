0a8b93c1f4d465d79c941ea1d029823110bc40da
Roman Onyshkiv <romaonyshkiv@gmail.com>
2025-07-27 19:57:57 -0400

    workflow

--- .github/scripts/scan_repos.py
+++ .github/scripts/scan_repos.py
@@ -1,12 +1,17 @@
 from datetime import datetime, timedelta
 from croniter import croniter
 import requests
+import yaml
 import os
 
 GIT_TOKEN = os.environ['GIT_TOKEN']
+REPOS = {
+    "NUNET": "Roman-inc/nunet"
+}
 
-BRANCHES_API = "https://https://api.github.com/repos/RomaOnyshkiv/{repo}/branches?per_page={per_page}&page={page}"
-TRIGGER_API = "https://api.github.com/repos/RomaOnyshkiv/multi_inf/actions/workflows/{workflow}/dispatches"
+RAW_FILE = "https://api.github.com/repos/{repo}/contents/.github/cron/schedule.yaml?ref={branch}"
+BRANCHES_API = "https://api.github.com/repos/{repo}/branches?per_page={per_page}&page={page}"
+TRIGGER_API = "https://api.github.com/repos/Roman-inc/build-cicd/actions/workflows/{workflow}/dispatches"
 
 HEADERS = {
     "Authorization": f'Bearer: {GIT_TOKEN}',
@@ -15,7 +20,7 @@ HEADERS = {
 
 def should_run(cron_expression):
     now = datetime.now()
-    prev_time = now - timedelta(minutes=60)
+    prev_time = now - timedelta(hours=4)
     next_run = croniter(cron_expression, prev_time).get_next(datetime)
     return prev_time < next_run <= now
 
@@ -43,3 +48,31 @@ def get_all_branches(repo):
             break
     print(f'found {len(all_branches)} in {repo}')
     return all_branches
+
+def scan_repo(repo_key):
+    repo = REPOS[repo_key]
+    all_inputs = {}
+    branches = get_all_branches(repo=repo)
+
+    for branch in branches:
+        r = requests.get(RAW_FILE.format(repo=repo, branch=branch), headers=HEADERS)
+        if r.status_code != 200:
+            continue
+
+        try:
+            schedule = yaml.safe_load(r.text)
+            for build in schedule.get('build', []):
+                cron_exp = build.get('cron')
+                if cron_exp and should_run(cron_exp):
+                    inputs = {
+                        'onefile': build['onefile']
+                    }
+                    all_inputs[build['name']] = inputs
+        except Exception as e:
+            print(f'Error in branch {branch}\n{e}')
+
+    for input_data in all_inputs:
+        trigger_workflow('build-nunet.yaml', input_data)
+
+def scan_all_repos():
+    scan_repo('NUNET')
